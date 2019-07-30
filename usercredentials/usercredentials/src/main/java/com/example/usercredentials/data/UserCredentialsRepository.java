a2411a1625eca53b3ce46416cb64eea19a889e92
unknown <aravindhankathiravan.iprimed@gmail.com>
2019-07-30 15:34:39 +0530

    Removing the directory

--- usercredentials/usercredentials/src/main/java/com/example/usercredentials/data/UserCredentialsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.usercredentials.data;
-
-import com.example.usercredentials.model.UserCredentials;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserCredentialsRepository extends MongoRepository<UserCredentials,String> {
-}
