a3262f350e2dc98190647b51456f01b2810c8b3c
saravananravikumar <52038862+saravananravikumar@users.noreply.github.com>
2019-07-31 15:18:12 +0530

    Add files via upload

---
+++ voizfonica/src/main/java/com/example/voizfonica/controller/PaymentController.java
@@ -0,0 +1,68 @@
+package com.payment.validcheck.Controller;
+
+import com.payment.validcheck.Data.PaymentRepository;
+import com.payment.validcheck.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/payment")
+public class PaymentController {
+
+    private PaymentRepository paymentRepo;
+
+    @Autowired
+    public PaymentController(PaymentRepository paymentRepo) {
+        this.paymentRepo = paymentRepo;
+    }
+
+    @ModelAttribute(name = "payment")
+    public Payment pay() {
+        return new Payment();
+    }
+
+    @GetMapping
+    public String showPaymentForm(Model model) {
+        Payment payment = new Payment();
+        model.addAttribute("payment", payment);
+        return "payment";
+    }
+
+    @PostMapping
+    public String processPaymentFrom(@Valid Payment payment, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            return "payment";
+        } else {
+            paymentRepo.save(payment);
+            model.addAttribute("hello", payment);
+            return "successpage";
+        }
+    }
+}
+
+/*
+
+    @PostMapping("/")
+    public String handlePostRequest(@Valid Payment payment, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "payment";
+        }
+    return "successpage";
+    }
+
+    @GetMapping("/")
+    public String handleGetRequest(Payment payment) {
+
+        return "payment";
+    }
+*/
+
+
