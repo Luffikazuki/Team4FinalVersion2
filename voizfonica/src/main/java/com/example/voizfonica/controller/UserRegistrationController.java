b8f3bc7ea435e6b6bcf28e476b785114a33921b5
Luffikazuki <52038802+Luffikazuki@users.noreply.github.com>
2019-08-01 15:56:10 +0530

    Updating changes in userRegistrationController

--- voizfonica/src/main/java/com/example/voizfonica/controller/UserRegistrationController.java
+++ voizfonica/src/main/java/com/example/voizfonica/controller/UserRegistrationController.java
@@ -1,22 +1,23 @@
 package com.example.voizfonica.controller;
 
 import com.example.voizfonica.data.UserCredentialRepository;
+import com.example.voizfonica.model.SubscriptionDetail;
 import com.example.voizfonica.model.UserCredential;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/customerRegister")
+@SessionAttributes("subscriptionDetail")
 public class UserRegistrationController {
     private UserCredentialRepository userCredentialRepository;
+
 
     @Autowired
     public UserRegistrationController(UserCredentialRepository userCredentialRepository){
@@ -41,8 +42,13 @@ public class UserRegistrationController {
             return "customerRegister";
         }else{
             userCredentialRepository.save(userCredential);
-            model.addAttribute("showDetails",userCredential);
-            return "redirect:/dashboard";
+            SubscriptionDetail subscriptionDetail = new SubscriptionDetail();
+            List<UserCredential> user1 = userCredentialRepository.findByEmailIdAndPassword(userCredential.getEmailId(),userCredential.getPassword());
+            subscriptionDetail.setUserId(user1.get(0).getId());
+            subscriptionDetail.setProductId(user1.get(0).getRequiredPlan());
+            model.addAttribute("subscriptionDetail",subscriptionDetail);
+
+            return "redirect:/chooseplan";
         }
     }
 
