b8c4c53dfab2cacaa6e11213b7865c18210126d8
nandhi20 <38161760+nandhi20@users.noreply.github.com>
2019-08-05 09:27:20 +0530

    Delete HeroRepository.java

--- hero/src/main/java/com/nandhirest/hero/HeroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.voizfonica.webapp.data;
-
-import com.voizfonica.webapp.Hero;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-@RepositoryRestResource(collectionResourceRel = "Favourite Hero List", path = "hero")
-public interface HeroRepository extends JpaRepository<Hero,Long> {
-
-}
