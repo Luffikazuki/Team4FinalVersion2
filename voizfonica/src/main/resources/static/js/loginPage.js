741b6c4ea68b1b4af21d0e3c63c6c2ba0e11e47e
Kishore Karthikeyan <kishorekart@gmail.com>
2019-08-12 21:38:39 +0530

    Update loginPage.js

--- voizfonica/src/main/resources/static/js/loginPage.js
+++ voizfonica/src/main/resources/static/js/loginPage.js
@@ -1,8 +1,18 @@
 function showPassword() {
     var x = document.getElementById("password");
-    if (x.type === "password") {
+    if (x.type == "password") {
         x.type = "text";
     } else {
         x.type = "password";
     }
 }
+
+function showConfirmPassword() {
+    var x = document.getElementById("confirm_password");
+    if (x.type == "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
